6b5529e8e55ea2aaef5277f24d98120aaf049d57
d11416146 <d11416146@gs.takming.edu.tw>
2026-06-24 14:04:40 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Person
+{
+	public string GetName()
+	{
+        return "許光漢";
+	}
+    public string GetName(string t)
+    {
+        return t+ "許光漢";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace finaltest_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            Console.WriteLine(p1.GetName());
+            Console.WriteLine(p1.GetName("德明"));
+        }
+    }
+}
